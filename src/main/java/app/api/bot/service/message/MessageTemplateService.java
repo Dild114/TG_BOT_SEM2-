56e25f84d190b1572c9ffe9186a69579e04fabc7
Sosnin_vadim <sosnin.vd@phystech.edu>
2025-04-30 01:21:46 +0300

    bd

--- src/main/java/app/api/bot/service/message/MessageTemplateService.java
+++ src/main/java/app/api/bot/service/message/MessageTemplateService.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageTemplateService {
   public String getWelcomeText(String userName) {
-    String filePath = "src\\main\\java\\app\\api\\bot\\message\\welcome.txt";
+    String filePath = "src/main/java/app/api/bot/message/welcome.txt";
     String welcomeText = FileUtils.readFileAsString(filePath).replace("{name}", userName);
     return welcomeText;
   }
 
   public String getHelpText() {
-    String filePath = "src\\main\\java\\app\\api\\bot\\message\\help.txt";
+    String filePath = "src/main/java/app/api/bot/message/help.txt";
     String helpText = FileUtils.readFileAsString(filePath);
     return helpText;
   }
