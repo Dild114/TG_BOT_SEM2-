18cecea9ca02ac0daa89769a42950e220f99f568
Sosnin_vadim <sosnin.vd@phystech.edu>
2025-05-03 14:43:52 +0300

    пофиксил вывод времени

--- src/main/java/app/api/bot/service/message/news/articles/ArticleMessageService.java
+++ src/main/java/app/api/bot/service/message/news/articles/ArticleMessageService.java
@@ -3,21 +3,27 @@ package app.api.bot.service.message.news.articles;
 import app.api.bot.service.MessageSenderService;
 import app.api.bot.service.keyboard.inlineKeyboard.ArticleMenuInlineKeyboard;
 
-import app.api.entity.*;
-import app.api.service.*;
+import app.api.entity.Article;
+import app.api.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 
+
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ArticleMessageService {
   private final ArticleMenuInlineKeyboard articleMenuInlineKeyboard;
   private final ArticleService articleService;
   private final MessageSenderService messageSenderService;
+
+  private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
   //TODO: заменить List<Article> на то, что будет нужно. Добавить удаление всех категорий из репо;
   public void sendArticles(long chatId, List<Article> articles) {
@@ -52,8 +58,8 @@ public class ArticleMessageService {
 
   //TODO: Возможно изменить/добавить какие-то эмодзи и тп, т.к. в заглушке всё по умолчанию красиво
   private String getMessageForArticleWithoutBrief(Article article) {
-    return "\uD83D\uDCCB " + article.getName() + "\n Категория: \"" + (article.getCategory() != null ? article.getCategory().getName() : "Без категории") + "\" \n\uD83D\uDCC5 "
-        + article.getCreationDate().toLocalDate() + " | \uD83D\uDD53 " + article.getCreationDate().toLocalTime();
+    return "\uD83D\uDCCB " + article.getName() + "\n Категория: \"" + (article.getCategory() != null ? article.getCategory().getName() : "Без категории")
+        + "\n\uD83D\uDCC5   " + article.getCreationDate().toLocalDate() + "     |    \uD83D\uDD53   " + article.getCreationDate().toLocalTime().format(TIME_FORMATTER);
   }
 
   private String getMessageForArticleWithBrief(Article article) {
@@ -63,7 +69,7 @@ public class ArticleMessageService {
     } else {
       brief = "Краткое содержание отсутствует \uD83E\uDD17";
     }
-    return "\uD83D\uDCCB " + article.getName() + "\n Категория: \"" + (article.getCategory() != null ? article.getCategory().getName() : "Без категории") + "\" \n\uD83D\uDCC5 "
-        + article.getCreationDate().toLocalDate() + " | \uD83D\uDD53 " + article.getCreationDate().toLocalTime() + "\n\n" + brief;
+    return "\uD83D\uDCCB " + article.getName() + "\n Категория: \"" + (article.getCategory() != null ? article.getCategory().getName() : "Без категории")
+        + "\n\uD83D\uDCC5   " + article.getCreationDate().toLocalDate() + "     |    \uD83D\uDD53   " + article.getCreationDate().toLocalTime().format(TIME_FORMATTER) + "\n\n" + brief;
   }
 }
